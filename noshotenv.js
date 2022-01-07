9393c3a6620abdf9d266293256a05ebdfe385cf5
Matt <carlotta.matt@gmail.com>
2022-01-07 12:49:09 -0800

    Update noshot tests

--- noshotenv.js
+++ noshotenv.js
@@ -4,17 +4,17 @@ const Test = require("./utils/tester");
 const test = new Test("@noshot/env", 6);
 
 // loading a single default .env
-test.start("single", 500000, () => {
+test.start("single", () => {
   env.config();
 });
 
 // large interpolated .env loading
-test.start("interpolated", 5000, () => {
+test.start("interpolated", () => {
   env.config({ paths: [".env.interp"] });
 });
 
 // loading multiple .env files (.env, .env.development, .env.local, .env.development.local)
-test.start("multiple", 500000, () => {
+test.start("multiple", () => {
   env.config({
     paths: [".env", ".env.development", ".env.local", ".env.development.local"]
   });
