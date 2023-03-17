e8a7131ffc9e045a10fcf5fbf3eecfe0979e2192
Matt <carlotta.matt@gmail.com>
2023-03-17 16:17:58 -0700

    chore: simplify tester and remove dependency

--- utils/createDate.js
+++ utils/createDate.js
@@ -1,14 +1,3 @@
-const dayjs = require("dayjs");
-
-const createDate = () => dayjs();
-
-const createDateWithFormat = () =>
-    createDate().format("dddd, MMMM D, YYYY h:mm A");
-
-const getSecondsDifference = date => createDate().diff(date, "s", true);
-
-module.exports = {
-    createDate,
-    createDateWithFormat,
-    getSecondsDifference
-};
+module.exports.createDateWithFormat = function() {
+    return new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'medium' }).format(new Date())
+}
--- utils/tester.js
+++ utils/tester.js
@@ -2,12 +2,7 @@ const { readFileSync, writeFileSync } = require("fs");
 const process = require("process");
 const readline = require("readline");
 const { join } = require("path");
-const {
-    createDate,
-    createDateWithFormat,
-    getSecondsDifference
-} = require("./createDate");
-const fileExists = require("./fileExists");
+const { createDateWithFormat } = require("./createDate");
 const iterations = require("../config/iterationsConfig");
 const runs = require("../config/runsConfig");
 
@@ -22,17 +17,17 @@ class Test {
      * @param {string} testName - name of test
      */
     constructor(testName) {
-        this.resultFile = join(process.cwd(), "result.json");
-        this.runIteration = 0;
-        this.runs = runs;
-        this.iterations = iterations;
-        this.testName = testName;
-        this.testRuns = [];
-        this.testResults = {};
-        this.testType = "";
-        this.runtimes = [];
         this.date = "";
         this.processenv = process.env;
+        this.resultFile = join(process.cwd(), "result.json");
+        this.runs = runs;
+        this.runtimes = [];
+        this.iterations = iterations;
+        this.testName = testName;
+        this.testResults = {};
+        this.testRuns = [];
+        this.testRunNum = 0;
+        this.testType = "";
 
         this.clearTerminal();
     }
@@ -50,14 +45,14 @@ class Test {
      * Prints a message to the terminal's `stdout`.
      * @function
      * @param {number} run - current test run
-     * @param {number} iteration - current test iteration
+     * @param {number} timeElapsed - time elapsed during run
      */
-    log(run, iteration, timeElapsed) {
+    log(run, timeElapsed) {
         terminal.clearLine(1);
         terminal.cursorTo(0);
         terminal.write(
-            `\x1b[32m[${this.testName}] test: ${this.testType} | run: ${run}/${this.runs
-            } | iteration: ${iteration}/${iteration
+            `\x1b[32m[${this.testName}] test name: ${this.testType} | run: ${run}/${this.runs
+            } | iterations: ${this.iterations[this.testRunNum - 1]
             } | elapsed: ${timeElapsed}s \x1b[0m\n`
         );
     }
@@ -68,39 +63,50 @@ class Test {
      * @param {function} testFn - the test function to run
      * @example ```test.start("test", () => {});```
      */
-    start(testType, testFn, idx) {
+    start({ testType, testFn }) {
         process.env = this.processenv;
+        this.date = createDateWithFormat();
         this.runtimes = [];
         this.testType = testType;
-        this.date = createDateWithFormat();
 
         for (let run = 1; run <= this.runs; run += 1) {
-            const startDate = createDate();
+            const startDate = new Date();
             terminal.write(`\nRunning ${testType} run ${run}... \n`);
+
             for (
                 let iteration = 0;
-                iteration <= this.iterations[idx];
+                iteration <= this.iterations[this.testRunNum - 1];
                 iteration += 1
             ) {
                 testFn();
             }
-            const timeElapsed = getSecondsDifference(startDate);
-            this.log(run, this.iterations[idx], timeElapsed);
+
+            const timeElapsed = (new Date().getTime() - startDate.getTime()) / 1000;
+            this.log(run, timeElapsed);
             this.runtimes.push(timeElapsed);
         }
 
         this.setResults();
     }
 
+    /**
+     * Adds a test function to `this.testRuns`.
+     * @param {string} testType - string name of test type
+     * @param {function} testFn - the test function to run
+     * @example ```test.add("test", () => {});```
+     */
     add(testType, testFn) {
         this.testRuns.push({ testType, testFn });
         return this;
     }
 
+    /**
+     * Runs added tests.
+     */
     run() {
-        for (let idx = 0; idx < this.testRuns.length; idx += 1) {
-            const { testType, testFn } = this.testRuns[idx];
-            this.start(testType, testFn, idx);
+        for (let runNum = 1; runNum < this.testRuns.length + 1; runNum += 1) {
+            this.testRunNum = runNum;
+            this.start(this.testRuns[this.testRunNum - 1]);
         }
     }
 
